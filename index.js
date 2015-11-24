d914637f3be916c8e6f9fa278e075b17b2a421ee
Mora <qiuzhongleiabc@126.com>
2015-11-24 21:10:33 +0800

    添加sass-lint

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ var fs = require('fs-extra'),
     glob = require('glob');
 
 var h = require('./lib/helper.js');
-var CS_ROOT = __dirname, PKG_ROOT, DEBUG = false;
-var ENGINES = ['jshint', 'jscs', 'eslint'];
+var CONFIG, CS_ROOT = __dirname, PKG_ROOT, DEBUG = false;
 
 function glueGlobPatterns(patterns) {
   if (patterns.length === 1) {
@@ -27,18 +26,15 @@ function init(opts) {
   DEBUG = !!opts.debug;
 
   // 将后缀名组装成 glob 可以直接使用的字段串
-  if (!opts.ext || opts.ext.length === 0) opts.ext = 'js';
+  if (!opts.ext || opts.ext.length === 0) opts.ext = '*';
   else opts.ext = glueGlobPatterns(opts.ext.map(function (ext) { return ext.replace(/^\./, ''); }));
 
-  // 没有指定使用任何的分析引擎，则使用默认的 jshint 和 jscs
-  if (ENGINES.every(function (k) { return !opts[k]; })) opts.jshint = opts.jscs = true;
+  // 没有指定使用任何的分析引擎，则自动根据文件后缀名来判断使用什么来 lint
+  if (Object.keys(CONFIG).every(function (k) { return !opts[k]; })) opts.auto = true;
 
-  ENGINES.forEach(function (key) {
-    var configKey = key + 'Config', rcFile = '.' + key + 'rc';
-
-    // 获取启用了的引擎的配置文件
-    if (!opts[configKey] && opts[key])
-      opts[configKey] = h.file(PKG_ROOT, rcFile) || path.join(CS_ROOT, rcFile);
+  Object.keys(CONFIG).forEach(function (key) {
+    var configKey = key + 'Config', name = CONFIG[key].config;
+    opts[configKey] = h.file(PKG_ROOT, name) || path.join(CS_ROOT, name);
   });
 }
 
@@ -51,12 +47,10 @@ function copy(src, dist) {
 
 function write(opts) {
   init(opts);
-
-  ENGINES.forEach(function (k) {
+  Object.keys(CONFIG).forEach(function (k) {
     if (opts[k]) {
       var src = path.resolve(opts[k + 'Config']),
         dist = path.resolve(path.join(PKG_ROOT, '.' + k + 'rc'));
-
       if (fs.existsSync(dist)) console.error('目标文件已经存在（' + dist + '）');
       else if (src === dist) console.error('目标文件和源文件地址一样（' + src + '）');
       else copy(src, dist);
@@ -64,7 +58,7 @@ function write(opts) {
   });
 }
 
-module.exports = function (opts) {
+function lint(opts) {
   init(opts);
 
   var appendExtToDir = function (all, current) {
@@ -85,25 +79,28 @@ module.exports = function (opts) {
   glob(
     patterns,
     { nodir: true, ignore: ignore },
-    function (err, files) { if (err) throw err; hint(files, opts); }
+    function (err, files) { if (err) throw err; lintFiles(files, opts); }
   );
-};
+}
 
-module.exports.write = write;
+function filterFiles(files, key) {
+  return files.filter(function (file) {
+    var ext = file.split('.').pop();
+    if (!ext) return false;
+    if (CONFIG[key].extensions.indexOf(ext) >= 0) return true;
+  });
+}
 
-function hint(files, opts) {
+function lintFiles(files, opts) {
 
-  if (!files.length) {
-    console.error('No js files!');
-    process.exit(1);
-  }
+  if (!files.length) return console.error('No lint files!');
 
   log('Found files: ', files, '\n');
 
   var tasks = [];
 
   function getBinFile(key) {
-    return require.resolve(path.join(key, 'bin', key));
+    return path.join(CS_ROOT, 'node_modules', '.bin', CONFIG[key].bin || key);
   }
 
   var verbose = DEBUG ? ['--verbose'] : [];
@@ -111,27 +108,49 @@ function hint(files, opts) {
   var injectArgs = {
     jshint: verbose.concat(jshintReporter),
     jscs: verbose,
-    eslint: []
+    eslint: [],
+    sasslint: ['--verbose'] // 总是 verbose
   };
 
-  ENGINES.forEach(function (key) {
-    if (opts[key]) {
+  Object.keys(CONFIG).forEach(function (key) {
+    if (opts[key] || opts.auto) {
+
+      var filtered = opts.auto ? filterFiles(files, key) : files;
+      if (!filtered.length) return false;
+
       tasks.push(function (done) {
         console.log('\r\nRunning ' + key + '...');
+
         var args = [getBinFile(key)].concat(injectArgs[key]);
         args.push('--config', opts[key + 'Config']);
-        args.push.apply(args, files);
+        args.push.apply(args, filtered);
         log('node ' + args.join(' '));
+
         h.spawn('node', args, function (code) {
-          if (code === 0 && key === 'eslint') console.log('No code style errors found.');
+          if (code === 0 && CONFIG[key].outputSuccess) console.log('No code style errors found.');
           if (code !== 0 && !opts.force) done(code);
           else done(null, code);
         });
+
       });
     }
   });
+
+  if (!tasks.length) return console.error('No lint tasks!');
 
   async.series(tasks, function (err, results) {
     process.exit(err || results.reduce(function (all, cur) { return all + cur; }, 0));
   });
 }
+
+module.exports = function (cfg) {
+  CONFIG = cfg;
+  return {
+    lint: lint,
+    write: write
+  };
+};
+
+
+
+
